9a3042d0e0b3d8fc24bf6e49947c2063c478f9ee
SundarDong <brosisstudio123@gmail.com>
2025-01-07 22:12:02 +0545

    feat: add flash screen

---
+++ lib/flashscreen/flashscreen1.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Flashscreen1 extends StatelessWidget {
+  const Flashscreen1({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
