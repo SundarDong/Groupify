e2bc7bda41153c3f33a718650464dd619129d8d1
SundarDong <brosisstudio123@gmail.com>
2025-01-08 11:57:40 +0545

    chore:directory of flashscreen and onboarding screen

---
+++ lib/onboarding/onboardingScreen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class Onboardingscreen extends StatelessWidget {
+  const Onboardingscreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        
+      ),
+    );
+  }
+}
